f06a8ad57ddd8658d92212525a72588d2fadbaf3
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-07 02:53:43 -0500

    docs: added seperator

--- content/meta.ts
+++ content/meta.ts
@@ -2,6 +2,9 @@ import type { MetaRecord } from 'nextra'
 
 export default {
   index: 'Overview',
+  
+  _ai: { type: 'separator', title: 'AI' },
+
   functions: 'Function',
   workflows: 'Workflow',
   database: 'Database',
